890c70c2c95cfd00b12b6c96b6eedb2bbbd6b04d
rlagusxo <rlagusxo@DESKTOP-M6P2TK9>
2022-12-29 15:36:59 +0900

    가자

---
+++ Day_two/src/Day_two.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class Day_two {
+
+	public static void main(String[] args) {
+//		//Scanner scanner = new Scanner(System.in);
+//		//String i = scanner.nextLine();
+//				
+////		System.out.println(i);
+//		//정수형 타입 
+//		//byte(1) ,short(2) ,int(4) ,long(8)
+//		byte by = -128;
+//		byte b1 = 127;
+//		
+//		
+//		short s1 = 128;
+//		short s2 = 32767;
+//		
+//		int i1 = 32768;
+//		int i2 = 213_765_234;
+////		System.out.println(i2);
+//		
+//		long l1 = 2_147_483_648L;
+//		float f1 = 3.141592f;
+//		double d1 = 3.141592;
+//		
+//		//논리 타입 
+//		//boolean (1 byte)
+//		boolean bool1 = true;
+//		boolean bool2 = false;
+//		boolean bool4 = true;
+//		
+//		
+//		//문자 타입 2byte
+//		char c1 = 'a';
+//		char c2 = 65;
+//		System.out.println(c2); //Ascii 코드로 출력 
+		
+		//입력 (System .in )
+//		java.util.Scanner 의 Scanner 객체 라이브러리를 사용 
+//		Scanner scanner = new Scanner(System.in); 
+//		int inputnum = scanner.nextInt();
+//		
+//		System.out.println(inputnum);
+//		char inputchar = scanner.next
+		
+		//묵시적 형변환 
+//		float f1 = 3.14f;
+//		byte b1 = 3;
+//		float f1 = b1;
+//		System.out.println(f1);
+//		
+//		int num = 100;
+//		byte by2 = (byte)num;
+//		
+//		System.out.println(by2);
+		
+//		int[] number = {10, 20, 30, 40, 50};
+		int[] number = new int[] {10, 20, 30, 40, 50};
+		
+//		for(int i=0;i<5;i++) {
+//			System.out.println(number[i]);
+//		}
+//		
+		for(int num:number) { //향상된 FOR문 
+			System.out.println(num);
+		}
+		
+		
+		
+		
+		
+
+	}
+
+}
